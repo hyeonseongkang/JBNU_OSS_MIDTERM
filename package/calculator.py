da21a9c2418f049ae4dcb6e76bd248c22bfe0994
HyeonSeongKang <gt9697@naver.com>
2022-11-04 18:03:59 +0900

    feat: ADD calculator.py 기존 main.py에 있던 계산하는 함수들을 package/calculator.py로 옮기고 main.py에서 calculator.py 불러와 사용

---
+++ package/calculator.py
@@ -0,0 +1,17 @@
+# This function adds two numbers
+def add(x, y):
+    return x + y
+
+# This function subtracts two numbers
+def subtract(x, y):
+    return x - y
+
+# This function multiplies two numbers
+def multiply(x, y):
+    return x * y
+
+#Need to define divide function.
+def divide (x,y):
+    if y == 0:
+        return False
+    return x / y
